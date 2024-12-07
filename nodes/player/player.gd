bcf755bf24bb671171c0263e03fc6fcecd2779e2
David <37672966+Ieida@users.noreply.github.com>
2024-12-07 15:43:59 +0100

    basic monster

--- nodes/player/player.gd
+++ nodes/player/player.gd
@@ -5,14 +5,15 @@ class_name Player extends CharacterBody2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down")
 	if direction:
 		velocity = direction * speed
-		if direction.y > 0.0: animated_sprite.play(&"face_down")
-		elif direction.y < 0.0: animated_sprite.play(&"face_up")
-		elif direction.x > 0.0: animated_sprite.play(&"face_right")
-		elif direction.x < 0.0: animated_sprite.play(&"face_left")
+		var a = rad_to_deg(Vector2.UP.angle_to(direction))
+		if absf(a) <= 45.0: animated_sprite.play(&"face_up")
+		elif absf(a) >= 135.0: animated_sprite.play(&"face_down")
+		elif a > 45.0 and a < 135.0: animated_sprite.play(&"face_right")
+		elif a < -45.0 and a > -135.0: animated_sprite.play(&"face_left")
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, speed)
 
